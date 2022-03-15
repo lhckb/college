d9a1c9cbd5f539417179ff2065617116e37bf731
lhckb <luis.carv.cruz@gmail.com>
2022-03-15 08:42:51 -0300

    improved pif exercise

--- 2022.1/pif/listas/aula04/geral/desafio3.c
+++ 2022.1/pif/listas/aula04/geral/desafio3.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 
+void printPos() 
+{
+  int count = 1;
+
+  printf("\n");
+  for (int i = 1; i < 4; i++) {
+    for (int j = 1; j < 3; j++) {
+      printf("%i", count++);
+    }
+    printf("\n");
+  }
+}
+
 int main()
 {
   int chute, vez = 0, flipped = 0, try = 1, pontos_j1 = 0, pontos_j2 = 0;
-  char board[2][2] = {{'a', 'b'}, {'b', 'a'}};
+  char board[3][2] = {{'a', 'b'}, {'b', 'a'}, {'c', 'c'}};
   char atual;
 
-  while (flipped < 2) {
+  do {
     if (vez % 2 == 0) {
       printf("vez do  jogador 1\n");
-      printf("diga uma posicao no quadro (1 a 4): ");
+      printf("diga uma posicao no quadro (1 a 6): ");
+      printPos();
       scanf("%i", &chute);
 
       switch (chute) {
@@ -30,7 +44,7 @@ int main()
                   }
                 }
               }
-              atual = 'c';
+              atual = 'z';
               pontos_j1++;
             }
           try = 1;
@@ -55,7 +69,7 @@ int main()
                   }
                 }
               }
-              atual = 'c';
+              atual = 'z';
               pontos_j1++;
             }
           try = 1;
@@ -80,7 +94,7 @@ int main()
                   }
                 }
               }
-              atual = 'c';
+              atual = 'z';
               pontos_j1++;
             }
           try = 1;
@@ -105,7 +119,57 @@ int main()
                   }
                 }
               }
-              atual = 'c';
+              atual = 'z';
+              pontos_j1++;
+            }
+          try = 1;
+          vez++;
+          }
+          break;
+
+        case 5:
+          if (try == 1) {
+            atual = board[2][0];
+            printf("seu chute é %c\n", board[2][0]);
+            try++;
+          }
+          else if (try == 2) {
+            printf("seu chute é %c\n", board[2][0]);
+            if (board[2][0] == atual) {
+              flipped++;
+              for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                  if (board[i][j] == atual) {
+                    board[i][j] = 'z';
+                  }
+                }
+              }
+              atual = 'z';
+              pontos_j1++;
+            }
+          try = 1;
+          vez++;
+          }
+          break;
+
+        case 6:
+          if (try == 1) {
+            atual = board[2][1];
+            printf("seu chute é %c\n", board[2][1]);
+            try++;
+          }
+          else if (try == 2) {
+            printf("seu chute é %c\n", board[2][1]);
+            if (board[2][1] == atual) {
+              flipped++;
+              for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                  if (board[i][j] == atual) {
+                    board[i][j] = 'z';
+                  }
+                }
+              }
+              atual = 'z';
               pontos_j1++;
             }
           try = 1;
@@ -118,7 +182,8 @@ int main()
 
     if (vez % 2 != 0) {
       printf("vez do  jogador 2\n");
-      printf("diga uma posicao no quadro (1 a 4): ");
+      printf("diga uma posicao no quadro (1 a 6): ");
+      printPos();
       scanf("%i", &chute);
 
       switch (chute) {
@@ -139,7 +204,7 @@ int main()
                   }
                 }
               }
-              atual = 'c';
+              atual = 'z';
               pontos_j2++;
             }
           try = 1;
@@ -164,7 +229,7 @@ int main()
                   }
                 }
               }
-              atual = 'c';
+              atual = 'z';
               pontos_j2++;
             }
           try = 1;
@@ -189,7 +254,7 @@ int main()
                   }
                 }
               }
-              atual = 'c';
+              atual = 'z';
               pontos_j2++;
             }
           try = 1;
@@ -214,8 +279,58 @@ int main()
                   }
                 }
               }
-              atual = 'c';
+              atual = 'z';
               pontos_j2++;
+            }
+          try = 1;
+          vez++;
+          }
+          break;
+
+        case 5:
+          if (try == 1) {
+            atual = board[2][0];
+            printf("seu chute é %c\n", board[2][0]);
+            try++;
+          }
+          else if (try == 2) {
+            printf("seu chute é %c\n", board[2][0]);
+            if (board[2][0] == atual) {
+              flipped++;
+              for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                  if (board[i][j] == atual) {
+                    board[i][j] = 'z';
+                  }
+                }
+              }
+              atual = 'z';
+              pontos_j1++;
+            }
+          try = 1;
+          vez++;
+          }
+          break;
+
+        case 6:
+          if (try == 1) {
+            atual = board[2][1];
+            printf("seu chute é %c\n", board[2][1]);
+            try++;
+          }
+          else if (try == 2) {
+            printf("seu chute é %c\n", board[2][1]);
+            if (board[2][1] == atual) {
+              flipped++;
+              for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 2; j++) {
+                  if (board[i][j] == atual) {
+                    board[i][j] = 'z';
+                  }
+                }
+              }
+              atual = 'z';
+              pontos_j1++;
             }
           try = 1;
           vez++;
@@ -223,7 +338,7 @@ int main()
           break;
       }
     }
-  } 
+  } while (flipped < 3);
 
   if (pontos_j1 > pontos_j2) {
     printf("vitória do jogador 1\n");
