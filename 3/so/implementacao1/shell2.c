3eb60c2ff44ee1eedd0ae23c5036256534d5231c
lhckb <luis.carv.cruz@gmail.com>
2022-10-04 11:58:38 -0300

    continue shell

--- 3/so/implementacao1/shell2.c
+++ 3/so/implementacao1/shell2.c
@@ -2,20 +2,33 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
 #define MAX_LINE 80 /* 80 chars per line, per command */
 
 int style = 0;
+int general_count;
+
+void removeNewLine(char *str) {
+  for (int i = 0; i < strlen(str); i++) {
+    if (str[i] == '\n') {
+      str[i] = 0;
+    }
+  }
+}
 
 void splitInputIntoComms(char *input, char *comms[MAX_LINE / 2 + 1]) {
 	int comm_count = 0, true = 1;
+  general_count = 0;
   char *piece;
 
   while (true) {
     piece = strtok_r(input, ";", &input);
     if (piece != NULL) {
+      // printf("%s\n", piece);
       comms[comm_count] = piece;
       comm_count++;
+      general_count++;
     }
     else {
       true = 0;
@@ -23,14 +36,14 @@ void splitInputIntoComms(char *input, char *comms[MAX_LINE / 2 + 1]) {
   }
 }
 
-void splitCommandIntoArgs(char *command, char *res[MAX_LINE / 2 + 1]) {
+void splitCommandIntoArgs(char *command, char *args[MAX_LINE / 2 + 1]) {
   int true = 1, arg_count = 0;
   char *piece;
 
   while (true) {
     piece = strtok_r(command, " ", &command);
     if (piece != NULL) {
-      res[arg_count] = piece;
+      args[arg_count] = piece;
       arg_count++;
     }
     else {
@@ -39,11 +52,18 @@ void splitCommandIntoArgs(char *command, char *res[MAX_LINE / 2 + 1]) {
   }
 }
 
-void executeSequential(char *command[]) {
-  printf("%s\n", command);
-  char *args[MAX_LINE / 2 + 1];
-  splitCommandIntoArgs(command, args);
-  printf("%s\n", args[0]);
+void executeSequential(char **command) {
+  // pid_t pid = fork();
+
+  // if (pid < 0) fprintf(stderr, "Fork failed.\n");
+  // if (pid == 0) wait(NULL);
+  // else {
+  //   // printf("%s\n", command[0]);
+  // }
+  printf("%s\n", command[0]);
+  printf("%s\n", command[1]);
+  // printf("%s\n", command[2]);
+  if (execvp(command[0], command) == -1) fprintf(stderr, "Execvp failed.\n");
 }
 
 int main(void)
@@ -60,13 +80,20 @@ int main(void)
 		fflush(stdout);
 
 		fgets(input, MAX_LINE, stdin);
-		printf("%s\n\n", input);
-
+		
 		if (!strcmp(input, "exit\n")) should_run = 0;
 		else {
       splitInputIntoComms(input, comms);
 
-      if (style == 0) executeSequential(comms);
+      if (style == 0) {
+        char *args[general_count];
+        for (int i = 0; i < general_count; i++) {
+          removeNewLine(comms[i]);
+          splitCommandIntoArgs(comms[i], args);
+          executeSequential(args);
+        }
+
+      } 
 		}
 
 		/*
