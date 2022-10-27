86d233035b000d0e681abe82a94aad2db0cacb35
lhckb <luis.carv.cruz@gmail.com>
2022-10-27 19:05:57 -0300

    continue rate

--- 3/so/implementacao2/rate.c
+++ 3/so/implementacao2/rate.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define NP 2
+#define NP 1000
 
 int num_processes = 0;
 
@@ -12,16 +12,39 @@ typedef struct process_st {
   int burst;
   int remaining_burst;
   char status;
-  struct process_st *next;
 } process_st;
 
-void insertProcessToList(process_st *processes, int burst, int period, char *name) {
+typedef struct log_st {
+  char name[10];
+  int runtime;
+  char status;
+} log_st;
+
+void swapProcesses(process_st *processes, int pos1) {
+  process_st temp = processes[pos1];
+  processes[pos1] = processes[pos1 + 1];
+  processes[pos1 + 1] = temp;
+}
+
+void sortByPriority(process_st *processes) {
+  for (int i = 0; i < num_processes; i++) {
+    for (int j = 0; j < num_processes - 1; j++) {
+      if (processes[j].period > processes[j + 1].period) {
+        swapProcesses(processes, j);
+      }
+    }
+  }
+}
+
+void insertProcessToList(process_st *processes, process_st process) {
   process_st new_process;
-  new_process.burst = burst;
-  new_process.period = period;
-  strcpy(new_process.name, name);
+  new_process.burst = process.burst;
+  new_process.period = process.period;
+  strcpy(new_process.name, process.name);
+  new_process.remaining_burst = process.remaining_burst;
   processes[num_processes] = new_process;
   num_processes++;
+  sortByPriority(processes);
 }
 
 void printList(process_st *processes) {
@@ -31,15 +54,26 @@ void printList(process_st *processes) {
   printf("==========\n\n");
 }
 
-void sortByPriority(process_st *processes) {
-  for (int i = 0; i < num_processes; i++) {
-    for (int j = 0; j < num_processes - 1; j++) {
-      if (processes[j].period > processes[j + 1].period) {
-        process_st temp = processes[j];
-        processes[j] = processes[j + 1];
-        processes[j + 1] = temp;
-      }
+int checkAndInsertIncomingProcess(process_st *processes, int elapsed_time) {
+  for (int i = 0; i < num_processes; i ++) {
+    if (elapsed_time % processes[i].period == 0) {
+      process_st incoming = processes[i];
+      insertProcessToList(processes, incoming);
+      return 1;
     }
+  }
+  return 0;
+}
+
+void logProcessState(log_st *logs, process_st process, int log_index, int interval) {
+  logs[log_index].status = process.status;
+  strcpy(logs[log_index].name, process.name);
+  logs[log_index].runtime = interval;
+}
+
+void printLogs(log_st *logs, int log_index) {
+  for (int i = 0; i < log_index; i ++) {
+    printf("[%s] for %d units - %c\n", logs[i].name, logs[i].runtime, logs[i].status);
   }
 }
 
@@ -50,28 +84,83 @@ int main(int argc, char *argv[]) {
   int total_time = 165;
 
   process_st processes[NP];
+  log_st logs[NP];
 
   // mock processes
-  insertProcessToList(processes, 25, 50, "t0");
-  insertProcessToList(processes, 35, 20, "t1");
+  process_st mock_process1;
+  mock_process1.burst = 25;
+  mock_process1.period = 50;
+  strcpy(mock_process1.name, "t1");
+  mock_process1.remaining_burst = mock_process1.burst;
+  insertProcessToList(processes, mock_process1);
 
-  sortByPriority(processes);
+  process_st mock_process2;
+  mock_process2.burst = 35;
+  mock_process2.period = 80;
+  strcpy(mock_process2.name, "t2");
+  mock_process2.remaining_burst = mock_process2.burst;
+  insertProcessToList(processes, mock_process2);
 
-  // printList(processes);
+  // process_st mock_process3;
+  // mock_process3.burst = 15;
+  // mock_process3.period = 50;
+  // strcpy(mock_process3.name, "t3");
+  // mock_process3.remaining_burst = mock_process3.burst;
+  // insertProcessToList(processes, mock_process3);
 
-  int processes_finished[NP];  // each index representes a process
-  int processes_lost[NP]; 
-  int processes_killed[NP]; 
+  printList(processes);
 
   int elapsed_time = 0;
   int interval = 0;
 
-  while (elapsed_time < total_time) {
-    //...
+  int exec_index = 0;
+  int log_index = 0;
+
+  process_st running;
+
+  while (elapsed_time < total_time && exec_index < num_processes) {
+    running = processes[exec_index];
+
+    if (interval >= processes[exec_index].burst) {
+      processes[exec_index].status = 'F';  // Finished
+      processes[exec_index].remaining_burst = processes[exec_index].burst - interval;
+      logProcessState(logs, processes[exec_index], log_index, interval);
+      log_index++;
+      interval = 0;
+      exec_index++;
+    }
+    if (elapsed_time > 0 && checkAndInsertIncomingProcess(processes, elapsed_time)) {
+
+      if (running.period > processes[exec_index].period) {
+        running.status = 'H';  // Halted
+        processes[exec_index].remaining_burst = processes[exec_index].burst - interval;
+        logProcessState(logs, running, log_index, interval);
+        log_index++;
+        interval = 0;
+      }
+      else if (!strcmp(running.name, processes[exec_index].name)) {
+        running.status = 'L';  //Lost
+        processes[exec_index].remaining_burst = processes[exec_index].burst - interval;
+        logProcessState(logs, running, log_index, interval);
+        log_index++;
+        exec_index++;
+        interval = 0;
+      }
+    }
+
+    processes[exec_index].status = 'R';  // Running
     
+    interval++;
 
     elapsed_time++;
   }
+
+  for (int i = exec_index; i < num_processes; i++) {
+    // printf("%s %d %c", );
+  }
+
+  printf("EXECUTION BY RATE\n");
+  printLogs(logs, log_index);
 
   return 0;
 }
