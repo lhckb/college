b908540497cb02134a5d4ba38dd2f03ca5f1a510
lhckb <luis.carv.cruz@gmail.com>
2022-10-27 15:20:30 -0300

    continue rate

--- 3/so/implementacao2/rate.c
+++ 3/so/implementacao2/rate.c
@@ -4,6 +4,8 @@
 
 #define NP 2
 
+int num_processes = 0;
+
 typedef struct process_st {
   char name[10];
   int period;
@@ -13,53 +15,49 @@ typedef struct process_st {
   struct process_st *next;
 } process_st;
 
-void freeAll(process_st *head) {
-  process_st *curr = head;
-  process_st *next;
-  while (curr != NULL) {
-    next = curr->next;
-    free(curr);
-    curr = next;
-  }
-  // free(curr);
-  free(next);
+void insertProcessToList(process_st *processes, int burst, int period, char *name) {
+  process_st new_process;
+  new_process.burst = burst;
+  new_process.period = period;
+  strcpy(new_process.name, name);
+  processes[num_processes] = new_process;
+  num_processes++;
 }
 
-void insertNewNode(process_st *head, int burst, int period, char *name) {
-  process_st *curr = head;
-  process_st *new = (process_st *) malloc(sizeof(process_st));
-  for (curr = head; curr->next != NULL; curr = curr->next);
-  curr->next = new;
-  new->burst = burst;
-  new->period = period;
-  strcpy(new->name, name);
-  new->next = NULL;
+void printList(process_st *processes) {
+  for (int i = 0; i < num_processes; i++) {
+    printf("%s %d %d\n", processes[i].name, processes[i].period, processes[i].burst);
+  }
+  printf("==========\n\n");
 }
 
-void printLinkedList(process_st *head) {
-  process_st *curr;
-  for (curr = head->next; curr != NULL; curr = curr->next) {
-    printf("%s %d %d\n", curr->name, curr->period, curr->burst);
+void sortByPriority(process_st *processes) {
+  for (int i = 0; i < num_processes; i++) {
+    for (int j = 0; j < num_processes - 1; j++) {
+      if (processes[j].period > processes[j + 1].period) {
+        process_st temp = processes[j];
+        processes[j] = processes[j + 1];
+        processes[j + 1] = temp;
+      }
+    }
   }
-  printf("==========\n");
 }
 
 int main(int argc, char *argv[]) {
-  if (argc > 1) printf("%s\n", argv[1]);
+  if (argc > 1) printf("%s\n\n", argv[1]);
 
   // mock time
   int total_time = 165;
 
-  process_st *head = (process_st *) malloc(sizeof(process_st));
-  head->next = NULL;
+  process_st processes[NP];
 
-  // mock process 0
-  insertNewNode(head, 25, 50, "t0");
+  // mock processes
+  insertProcessToList(processes, 25, 50, "t0");
+  insertProcessToList(processes, 35, 20, "t1");
 
-  // mock process 1
-  insertNewNode(head, 35, 80, "t1");
+  sortByPriority(processes);
 
-  printLinkedList(head);
+  // printList(processes);
 
   int processes_finished[NP];  // each index representes a process
   int processes_lost[NP]; 
@@ -70,11 +68,10 @@ int main(int argc, char *argv[]) {
 
   while (elapsed_time < total_time) {
     //...
+    
 
     elapsed_time++;
   }
-
-  // freeAll(head);
 
   return 0;
 }
